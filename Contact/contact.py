1a20aee6417001f2e035a5dbe1043fa513899d07
infomuscle <infomuscle10@gmail.com>
2019-12-16 15:12:24 +0900

    08-2-5 연락처 출력하기

--- Contact/contact.py
+++ Contact/contact.py
@@ -18,6 +18,10 @@ def set_contact():
     addr = input("Address: ")
     contact = Contact(name, phone_number, e_mail, addr)
     return contact
+
+def print_contact(contact_list):
+    for contact in contact_list:
+        contact.print_info()
     
 def print_menu():
     print("1. 연락처 입력")
@@ -34,6 +38,8 @@ def run():
         if menu == 1:
             contact = set_contact()
             contact_list.append(contact)
+        elif menu == 2:
+            print_contact(contact_list)
         elif menu == 4:
             break
 
