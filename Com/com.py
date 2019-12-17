39d17202715467ca065a4ff0411582dbe57a2c12
infomuscle <infomuscle10@gmail.com>
2019-12-17 14:23:40 +0900

    09-2 파이썬으로 엑셀 다루기

--- Com/com.py
+++ Com/com.py
@@ -13,8 +13,17 @@ instCpStockCode = CpStockCode()
 print(instCpStockCode.GetCount())
 print(instCpStockCode.NameToCode('유한양행'))
 
-explore = win32com.client.Dispatch("InternetExplorer.Application")
-explore.Visible = True
+# explore = win32com.client.Dispatch("InternetExplorer.Application")
+# explore.Visible = True
+#
+# word = win32com.client.Dispatch("Word.Application")
+# word.Visible = True
 
-word = win32com.client.Dispatch("Word.Application")
-word.Visible = True
+excel = win32com.client.Dispatch("Excel.Application")
+excel.Visible = True
+
+wb = excel.Workbooks.Add()
+ws = wb.Worksheets("Sheet1")
+ws.Cells(1,1).Value = "hello world"
+wb.SaveAs('test1.xlsx')
+excel.Quit()
